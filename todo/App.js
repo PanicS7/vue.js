1d7baab96039336a8879ef104e2a51cf4b5259b4
Sanja Panic <55251860+PanicS7@users.noreply.github.com>
2022-02-02 21:03:38 +0100

    Added todo app
    Simple todo app in vue.js

---
+++ todo/App.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+    data() {
+        return {
+            message: "",
+            todos: []
+        }
+    },
+    methods: {
+        addTodo() {
+            // if message is not empty
+            if (this.message !== "") {
+                // update list and set message to blank
+                this.todos = [...this.todos,{value: this.message, checked: false}];
+                this.message = "";
+            }                                      
+        },
+        handleClose(index) {
+            // remove selected todo from list
+            this.todos.splice(index,1);
+        }
+    }
+ 
+}).mount("#root");
